03cab0aec5e6ef675a373880b48734fafffe52ea
leandro-miranda <leandromiranda2010@gmail.com>
2022-12-23 18:10:35 -0300

    Refatorando exercício 2

--- 02-valorJantar.js
+++ 02-valorJantar.js
@@ -18,7 +18,7 @@ let valorJantar = 80;
 let taxaGarcom = 10;
 let total = 0;
 
-total = 80 + ((taxaGarcom / 100) * 80);
+total = valorJantar + ((taxaGarcom / 100) * valorJantar);
 
 // console.log('Valor do jantar: R$' + valorJantar + ',00');
 console.log(`Valor do jantar: R$${valorJantar},00`);
