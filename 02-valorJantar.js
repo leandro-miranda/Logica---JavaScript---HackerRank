009b015d315a06ddfea14d94adfaa7b0c9bb9f07
leandro-miranda <leandromiranda2010@gmail.com>
2022-12-23 18:08:48 -0300

    Exercício 2

--- 02-valorJantar.js
+++ 02-valorJantar.js
@@ -6,22 +6,22 @@
 // Taxa do garçom: R$8,00
 // Total a pagar: R$88,00
 
-let valorJantar = 80;
-let taxaGarcom = valorJantar * 0.1;
-let valorTotal = valorJantar + taxaGarcom;
-
-console.log(`Valor do jantar: R$${valorJantar},00`);
-console.log(`Taxa do garçom: R$${taxaGarcom},00`);
-console.log(`Total a pagar: R$${valorTotal},00`);
-
 // let valorJantar = 80;
-// let taxaGarcom = 10;
-// let total = 0;
+// let taxaGarcom = valorJantar * 0.1;
+// let valorTotal = valorJantar + taxaGarcom;
 
-// total = 80 + ((taxaGarcom / 100) * 80);
+// console.log(`Valor do jantar: R$${valorJantar},00`);
+// console.log(`Taxa do garçom: R$${taxaGarcom},00`);
+// console.log(`Total a pagar: R$${valorTotal},00`);
+
+let valorJantar = 80;
+let taxaGarcom = 10;
+let total = 0;
+
+total = 80 + ((taxaGarcom / 100) * 80);
 
 // console.log('Valor do jantar: R$' + valorJantar + ',00');
-// console.log(`Valor do jantar: R$${valorJantar},00`);
-// console.log(`Taxa do garçom: R$${(taxaGarcom / 100) * 80},00`);
-// console.log(`Total a pagar: R$${total},00`);
+console.log(`Valor do jantar: R$${valorJantar},00`);
+console.log(`Taxa do garçom: R$${(taxaGarcom / 100) * 80},00`);
+console.log(`Total a pagar: R$${total},00`);
 
